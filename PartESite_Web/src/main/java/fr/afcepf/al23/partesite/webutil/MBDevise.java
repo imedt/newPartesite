d286e9bee5e490e3b8dd0d47f424d98d18e60035
graffiti972 <ahvoila@live.fr>
2015-04-17 15:16:47 +0200

    blabla

---
+++ PartESite_Web/src/main/java/fr/afcepf/al23/partesite/webutil/MBDevise.java
@@ -0,0 +1,60 @@
+package fr.afcepf.al23.partesite.webutil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+
+import org.apache.log4j.Logger;
+
+import fr.afcepf.al23.partesite.managedbean.MBConnexion;
+
+@ManagedBean(name = "mbDevise")
+@SessionScoped
+public class MBDevise {
+	private static Logger log = Logger.getLogger(MBDevise.class);
+	@ManagedProperty(value = "#{mbConnexion}")
+	private MBConnexion MBCnx;
+	private List<String> list;
+
+	public List getListDevise() {
+		log.info("devise : "+MBCnx.getDevise());
+		list = new ArrayList<String>();
+		list.add("EUR");
+		list.add("BAM");
+		list.add("BGN");
+		list.add("HRK");
+		list.add("CZK");
+		list.add("DKK");
+		list.add("GBP");
+		list.add("HUF");
+		list.add("MKD");
+		list.add("NOK");
+		list.add("PLN");
+		list.add("RON");
+		list.add("RSD");
+		list.add("SEK");
+		list.add("CHF");
+		list.add("TRY");
+		return list;
+	}
+
+	public MBConnexion getMBCnx() {
+		return MBCnx;
+	}
+
+	public void setMBCnx(MBConnexion mBCnx) {
+		MBCnx = mBCnx;
+	}
+
+	public List<String> getList() {
+		return list;
+	}
+
+	public void setList(List<String> list) {
+		this.list = list;
+	}
+	
+}
